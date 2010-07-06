b7d394b391f47a4338de6ad3198cc70de5b57e66
Mike Perham <mperham@gmail.com>
2010-07-05 19:25:37 -0700

    Create databases for each wordpress instance

--- chef/site-cookbooks/base/recipes/wordpress.rb
+++ chef/site-cookbooks/base/recipes/wordpress.rb
@@ -9,3 +9,11 @@ cookbook_file '/etc/apache2/sites-available/wordpress.conf'
 # link '/etc/apache2/sites-enabled/wordpress' do
 #   to '/etc/apache2/sites-available/wordpress.conf'
 # end
+
+
+
+%w(perham mikeperham).each do |db|
+  execute "create-database-#{db}" do
+    command "/usr/bin/mysqladmin -u root -ppassword create database if not exists #{db}"
+  end
+end
